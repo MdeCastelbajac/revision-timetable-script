5e1f285e79beba5df08b4efa3eb2d8aab754a818
Matthieu de Castelbajac <mathieu.decastelbajac@gmail.com>
2020-10-06 16:57:58 +0200

    Update timetable.py

--- timetable.py
+++ timetable.py
@@ -1,18 +1,4 @@
 #!/usr/bin/python
-#
-#           _                                 _   _            
-#          | |                               | | (_)           
-#  _ __ ___| |_ _ __ ___  ___ _ __   ___  ___| |_ ___   _____  
-# | '__/ _ \ __| '__/ _ \/ __| '_ \ / _ \/ __| __| \ \ / / _ \ 
-# | | |  __/ |_| | | (_) \__ \ |_) |  __/ (__| |_| |\ V /  __/ 
-# |_|  \___|\__|_|_ \___/|___/ .__/ \___|\___|\__|_| \_/ \___| 
-# | |      | |   | |         | |                               
-# | |_ __ _| |__ | | ___     |_|                               
-# | __/ _` | '_ \| |/ _ \                                      
-# | || (_| | |_) | |  __/                                      
-#  \__\__,_|_.__/|_|\___|                                      
-#                                                              
-#                                                              
 
 # script requirements
 import sys
@@ -26,7 +12,7 @@ from openpyxl.styles import PatternFill, Font
 # insert date
 from datetime import date
 
-# CONSTANT : common path to spreadsheets : 
+# CONSTANT : common path to csv's : 
 commonPath = "Licence/TimeTables/"
 
 
